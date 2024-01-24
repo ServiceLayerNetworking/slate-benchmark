4d53088da946ed31fa15c3ddeefed0aae184befb
gangmuk <gangmuk@node0.gangmuk-186812.istio-pg0.utah.cloudlab.us>
2024-01-24 05:21:47 +0000

    Update curl.sh

--- matmul-app/curl.sh
+++ matmul-app/curl.sh
@@ -1,16 +1,22 @@
-method=$1
-cluster=$2
+cluster=$1
+method=$2
+
+node1="node1.gangmuk-186812.istio-pg0.utah.cloudlab.us"
+node2="node2.gangmuk-186812.istio-pg0.utah.cloudlab.us"
+nodeport=30277
+#nodeport=31024
+
 
 if [ $cluster == "west" ]; then
     if [ $method == "GET" ]; then
-        curl -v -X ${method} -H "x-slate-destination: west" http://node1.gangmuk-185120.istio-pg0.clemson.cloudlab.us:30763/compute\?row\=2\&column\=2
+        curl -v -X ${method} -H "x-slate-destination: west" ${node1}:${nodeport}/compute\?row\=2\&column\=2
     elif [ $method == "POST" ]; then
-        curl -v -X ${method} -H "x-slate-destination: west" http://node1.gangmuk-185120.istio-pg0.clemson.cloudlab.us:30763/compute\?row\=1000\&column\=1000
+        curl -v -X ${method} -H "x-slate-destination: west" ${node1}:${nodeport}/compute\?row\=1000\&column\=1000
     fi
 elif [ $cluster == "east" ]; then
     if [ $method == "GET" ]; then
-        curl -v -X ${method} -H "x-slate-destination: east" http://node2.gangmuk-185120.istio-pg0.clemson.cloudlab.us:30763/compute\?row\=2\&column\=2
+        curl -v -X ${method} -H "x-slate-destination: east" ${node2}:${nodeport}/compute\?row\=2\&column\=2
     elif [ $method == "POST" ]; then
-        curl -v -X ${method} -H "x-slate-destination: east" http://node2.gangmuk-185120.istio-pg0.clemson.cloudlab.us:30763/compute\?row\=1000\&column\=1000
+        curl -v -X ${method} -H "x-slate-destination: east" ${node2}:${nodeport}/compute\?row\=1000\&column\=1000
     fi
 fi
