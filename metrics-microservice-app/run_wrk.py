bd11ba27cd5f967e175e674ba002a985dd2e8d92
gangmuk@gmail.com <gangmuk@gmail.com>
2024-03-13 01:26:42 +0000

    Update metrics-app run_wrk.py to record historical routing rule data

--- metrics-microservice-app/run_wrk.py
+++ metrics-microservice-app/run_wrk.py
@@ -19,12 +19,43 @@ CONFIG = {
     'distribution': 'exp',
     'thread': 50,
     'connection': 50,
-    'duration': 60
+    'duration': 30
 }
 
+def get_pod_name_from_deploy(deployment_name, namespace='default'):
+    config.load_kube_config()
+    api_instance = client.AppsV1Api()
+    try:
+        pod_name = run_command(f"kubectl get pods -l app={deployment_name} -o custom-columns=:metadata.name")
+        return pod_name
+    except client.ApiException as e:
+        print(f"Error fetching the pod name for deployment {deployment_name}: {e}")
+        assert False
+
+def kubectl_cp_from_slate_controller_to_host(src_in_pod, dst_in_host):
+    slate_controller_pod = get_pod_name_from_deploy("slate-controller")
+    print(f"Try kubectl_cp_from_slate_controller_to_host")
+    print(f"src_in_pod: {slate_controller_pod}:{src_in_pod}")
+    print(f"dst_in_host: {dst_in_host}")
+    # slate_controller_pod = run_command("kubectl get pods -l app=slate-controller -o custom-columns=:metadata.name")
+    temp_file = "temp_file.txt"
+    run_command(f"kubectl cp {slate_controller_pod}:{src_in_pod} {temp_file}")
+    run_command(f"mv {temp_file} {dst_in_host}")
+    print(f"mv {temp_file} {dst_in_host}")
+    print(f"finish kubectl_cp_from_slate_controller_to_host")
+    
+
+def kubectl_cp_from_host_to_slate_controller_pod(src_in_host, dst_in_pod):
+    slate_controller_pod = run_command("kubectl get pods -l app=slate-controller -o custom-columns=:metadata.name")
+    print(f"Try kubectl_cp_from_host_to_slate_controller_pod")
+    print(f"src_in_host: {src_in_host}")
+    print(f"dst_in_pod: {slate_controller_pod}:{dst_in_pod}")
+    run_command(f"kubectl cp {src_in_host} {slate_controller_pod}:{dst_in_pod}")
+    print(f"finish kubectl_cp_from_host_to_slate_controller_pod")
+    
 
 def update_env_txt(mode, benchmark_name, total_num_services, routing_rule, rps_dict, inter_cluster_latency):
-    print("update_env")
+    print("update_env_txt")
     global CONFIG
     CONFIG["mode"] = mode
     CONFIG["routing_rule"] = routing_rule
@@ -33,48 +64,27 @@ def update_env_txt(mode, benchmark_name, total_num_services, routing_rule, rps_d
     CONFIG["inter_cluster_latency"] = inter_cluster_latency
     CONFIG["benchmark_name"] = benchmark_name
     CONFIG["total_num_services"] = total_num_services
-   
    # File to update
-    local_env_file = "env.txt"
+    local_env_file = "local_env.txt"
     with open(local_env_file, "w") as file:
         for key, value in CONFIG.items():
-            file.write(f"{key},{value}\n")
-            
-    # Get the pod name
-    try:
-        result = subprocess.run(["kubectl", "get", "pods", "-l", "app=slate-controller", "-o", "custom-columns=:metadata.name"], capture_output=True, text=True, check=True)
-        slate_controller_pod = result.stdout.strip()
-    except subprocess.CalledProcessError as e:
-        print(f"Error fetching the slate-controller pod: {e}")
-        sys.exit()
-
-    # Copy the file to the pod
-    try:
-        subprocess.run(["kubectl", "cp", local_env_file, f"{slate_controller_pod}:/app/env.txt"], check=True)
-        print(f"kubectl cp {local_env_file} {slate_controller_pod}:/app/env.txt")
-    except subprocess.CalledProcessError as e:
-        print(f"Error copying {local_env_file} to the pod: {e}")
-        sys.exit()
-    # Optionally, print the contents of the file
-    # try:
-    #     with open(local_env_file, "r") as file:
-    #         print(file.read())
-    # except IOError as e:
-    #     print(f"Error reading file {local_env_file} after modification: {e}")
-    #     sys.exit()
+            file.write(f"{key},{value}\n") 
+    print(f"write {local_env_file}")
+    return local_env_file
         
 '''
-slate_controller/env.txt => local file
+slate_controller/app/env.txt => local file
 '''
-def scp_env_txt(output_path):
-    print(f"start scp_env_txt")
-    slate_controller_pod = run_command("kubectl get pods -l app=slate-controller -o custom-columns=:metadata.name")
-    src_path = "/app/env.txt"
-    temp_file = "temp_env.txt" # This is for debugging. 
-    run_command(f"kubectl cp {slate_controller_pod}:{src_path} {temp_file}")
+# def scp_env_txt(output_path):
+    # print(f"start scp_env_txt")
+    # slate_controller_pod = run_command("kubectl get pods -l app=slate-controller -o custom-columns=:metadata.name")
+    # src_path = "/app/env.txt"
+    # temp_file = "temp_env.txt" # This is for debugging. 
+    # run_command(f"kubectl cp {slate_controller_pod}:{src_path} {temp_file}")
     # run_command(f"rm {temp_file}")
-    print(f"src_env_file: {slate_controller_pod}:{src_path}")
-    print(f"update {output_path} with env.txt")
+    # print(f"src_env_file: {slate_controller_pod}:{src_path}")
+    # print(f"update {output_path} with env.txt")
+    
 
 # def scrape_resource_config():
 #     config.load_kube_config()
@@ -222,7 +232,8 @@ def run_command(command):
         return output.decode('utf-8').strip()
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {e.output.decode('utf-8')}")
-        exit(1)
+        print("Assertion! Exit...")
+        exit()
     
 
 def run_wrk(copy_config, target_cluster, req_type, target_cluster_rps, wrk_log_path):
@@ -242,7 +253,7 @@ def run_wrk(copy_config, target_cluster, req_type, target_cluster_rps, wrk_log_p
     copy_config["cluster"] = target_cluster
     copy_config["RPS"] = target_cluster_rps
     
-    print(f'tart {req_type} RPS {target_cluster_rps} to {target_cluster} cluster for {copy_config["duration"]} seconds')
+    print(f'start {req_type} RPS {target_cluster_rps} to {target_cluster} cluster for {copy_config["duration"]} seconds')
     with open(wrk_log_path, "a") as f:
         info = "-- start of config --\n"
         for key, value in copy_config.items():
@@ -349,16 +360,21 @@ def restart_slate_controller():
     print("@@ kubectl rollout restart deploy slate-controller")
     sleep_and_print(10)
 
-def scp_trace_string_file(output_path):
-    print(f"start scp_trace_string_file")
-    slate_controller_pod = run_command("kubectl get pods -l app=slate-controller -o custom-columns=:metadata.name")
-    src_path = "/app/trace_string.csv"
-    print(f"src_env_file: {slate_controller_pod}:{src_path}")
-    temp_path = "temp.slatelog"
-    print(f"dst: {output_path}")
-    run_command(f"kubectl cp {slate_controller_pod}:{src_path} {temp_path}")
-    run_command(f"mv {temp_path} {output_path}")
-    print(f"scp_trace_string_file is done")
+'''
+slate_controller/app/trace_string.csv -> local file
+'''
+# def scp_trace_string_file(output_path):
+#     print(f"start scp_trace_string_file")
+#     slate_controller_pod = run_command("kubectl get pods -l app=slate-controller -o custom-columns=:metadata.name")
+#     src_path = "/app/trace_string.csv"
+#     print(f"src_env_file: {slate_controller_pod}:{src_path}")
+#     temp_path = "temp.slatelog"
+#     print(f"dst: {output_path}")
+#     run_command(f"kubectl cp {slate_controller_pod}:{src_path} {temp_path}")
+#     run_command(f"mv {temp_path} {output_path}")
+#     print(f"scp_trace_string_file is done")
+    
+    
 
 def main():
     start_time = datetime.now()
@@ -376,10 +392,21 @@ def main():
     '''
     benchmark_name="metrics"
     total_num_services=4
+    '''
+    profile: collecting traces, routing rule is always local
+    runtime: training latency functions with the given trace data (trace_string.csv)
+    '''
     mode_and_routing_rule = {\
-        "profile": ["LOCAL"],\
-        # "runtime": ["LOCAL", "SLATE", "MCLB"],\
+        # "profile": ["LOCAL"],\
+        "runtime": ["WATERFALL"],\
+        # "runtime": ["LOCAL", "SLATE", "MCLB",  "WATERFALL"],\
+        # "runtime": ["LOCAL", "SLATE", "MCLB",  "WATERFALL", "REMOTE"],\
     }
+    
+    mode_set = ["profile", "runtime"]
+    for mode in mode_and_routing_rule:
+        assert mode in mode_set
+        
     # oneway_latency = [0]
     oneway_latency = [20]
     req_type_list = ["get_metric"] # It should match wrk2 lua script
@@ -390,12 +417,12 @@ def main():
     west_rps_list["get_metrics][1], east_rps_list["get_metrics][1] are pair
     '''
     all_RPS_list = [ \
-                    {"west": 50, "east": 0}, \
-                    # {"west": 100, "east": 0}, \
+                    # {"west": 50, "east": 0}, \
+                    {"west": 100, "east": 0}, \
                     # {"west": 150, "east": 0}, \
-                    {"west": 200, "east": 0}, \
-                    {"west": 250, "east": 0}, \
-                    {"west": 300, "east": 0}, \
+                    # {"west": 200, "east": 0}, \
+                    # {"west": 250, "east": 0}, \
+                    # {"west": 300, "east": 0}, \
                     ]
     ########################################################
     
@@ -421,11 +448,13 @@ def main():
             for mode in mode_and_routing_rule:
                 for routing_rule in mode_and_routing_rule[mode]:
                     date_ = datetime.now().strftime("%b%d")
-                    postfix = f"{date_}_afternoon"
+                    postfix = f"{date_}_testing"
                     print(f"* postfix for output file: {postfix}")
                     for req_type in req_type_list:
                         for rps_dict in all_RPS_list:
                             per_wrk_st = datetime.now()
+                            
+                            ''' Initialize the output path for different logs'''
                             if istio_config:
                                 if wasm_config:
                                     output_dir = f"{req_type}-ww-{postfix}"
@@ -435,62 +464,74 @@ def main():
                                 output_dir = f"{req_type}-woi-{postfix}"
                             output_dir += "/"
                             for cluster in rps_dict:
+                                # cluster[0]: either 'w' or 'e'
                                 output_dir += f"{cluster[0]}{rps_dict[cluster]}"
+                            # e.g., output_dir: get_metric-ww-Mar13_testing/w200e100
                             assert output_dir != ""
                             if not os.path.isdir(output_dir):
                                 os.makedirs(output_dir)
-                            update_env_txt(mode, benchmark_name, total_num_services, routing_rule, rps_dict, lat)
-                            time.sleep(1)
-                            
-                            print(f"* mode: {mode}, routing_rule: {routing_rule}")
-                            
                             formatted_datetime = datetime.now().strftime("%m-%d-%H:%M")
-                            common_log_name_dict = dict()
                             wrk_log_path_dict = dict()
-                            slate_log_path_dict = dict()
-                            for cluster in all_RPS_list[0]:
-                                common_log_name_dict[cluster] = f"{output_dir}/{mode}-L{lat}-{routing_rule}-R{rps_dict[cluster]}-{cluster}-{formatted_datetime}"
-                            for cluster in common_log_name_dict:
-                                wrk_log_path_dict[cluster] = f"{common_log_name_dict[cluster]}.wrklog"
-                            for cluster in common_log_name_dict:
-                                slate_log_path_dict[cluster] = f"{common_log_name_dict[cluster]}.slatelog"
-                                
+                            for cluster in rps_dict:
+                                wrk_log_path_dict[cluster] = f"{output_dir}/{mode}-L{lat}-{routing_rule}-R{rps_dict[cluster]}-{cluster}-{formatted_datetime}.wrklog"
+                            
+                            ''' update env.txt and scp to slate-controller pod '''
+                            local_env_file = update_env_txt(mode, benchmark_name, total_num_services, routing_rule, rps_dict, lat)
+                            kubectl_cp_from_host_to_slate_controller_pod(local_env_file, "/app/env.txt")
+                            time.sleep(1)
+                            # This is only for debugging to confirm that env.txt is transferred to the slate-controller pod correctly by copying back from slate-controller pod to the host                            
                             for cluster in wrk_log_path_dict:
-                                scp_env_txt(wrk_log_path_dict[cluster])
+                                kubectl_cp_from_slate_controller_to_host("/app/env.txt", f"temp-env-{cluster}.txt")
                             time.sleep(1)
                             
+                            ''' start to send load concurrently to all clusters'''
                             with concurrent.futures.ThreadPoolExecutor() as executor:
                                 for cluster in wrk_log_path_dict:
-                                    print(f"cluster: {cluster}, req_type: {req_type}, RPS: {rps_dict[cluster]}, wrk_log_path: {wrk_log_path_dict[cluster]}")
-                                future_list = list()
+                                    print(f"cluster: {cluster}, req_type: {req_type}, RPS: {rps_dict[cluster]}")
+                                    # print(f"wrk_log_path: {wrk_log_path_dict[cluster]}")
+                                    
                                 global CONFIG
-                                copy_config = copy.deepcopy(CONFIG)
+                                copy_config = copy.deepcopy(CONFIG) # to avoid concurrency issue
+                                future_list = list()
                                 for cluster in wrk_log_path_dict:
                                     future_list.append(executor.submit(run_wrk, copy_config, cluster, req_type, rps_dict[cluster], wrk_log_path_dict[cluster]))
                                 
-                                # lapse: 0s
-                                first_sleep = 10
-                                time.sleep(first_sleep)
-                                print(f"first sleep for {first_sleep} seconds before recording resource allocation")
-                                # lapse: 10s
+                                ''' record resource allocation '''
+                                time.sleep(1)
                                 for cluster in wrk_log_path_dict:
                                     record_pod_resource_allocation(wrk_log_path_dict[cluster], rps_dict[cluster])
-                                
-                                second_sleep = CONFIG['duration']-(first_sleep+10)
-                                time.sleep(second_sleep)
-                                print(f"second sleep for {second_sleep} seconds before resource resource usage")
-                                # lapse: 50s
+                                    
+                                ''' record resource usage 20 before the end of the experiment '''
+                                sleep_before_resource_usage_recording = CONFIG['duration'] - 20
+                                time.sleep(sleep_before_resource_usage_recording)
+                                print(f"sleep for {sleep_before_resource_usage_recording} seconds before resource resource usage")
                                 for cluster in wrk_log_path_dict:
                                     record_pod_resource_usage(wrk_log_path_dict[cluster], rps_dict[cluster])
-                                
+                                    
+                                ''' Join all threads '''
                                 for future in concurrent.futures.as_completed(future_list):
                                     print(future.result())
-                            print("Both functions have completed.")
+                                    
+                            print("Both wrk2 have completed.")
                             
-                            for cluster in slate_log_path_dict:
-                                scp_trace_string_file(slate_log_path_dict[cluster])
-                            
-                            restart_deploy(exclude=[])
+                            if mode == "profile":
+                                slate_log_path = f"{output_dir}/trace.slatelog"
+                                kubectl_cp_from_slate_controller_to_host("/app/trace_string.csv",{slate_log_path})
+                            elif mode == "runtime":
+                                src_in_pod = "/app/sim_percentage_df_most_recent.csv"
+                                dst_in_host = f'{output_dir}/{src_in_pod.split("/")[-1]}'
+                                kubectl_cp_from_slate_controller_to_host(src_in_pod, dst_in_host)
+                                
+                                src_in_pod = "/app/sim_percentage_df_history.csv"
+                                dst_in_host = f'{output_dir}/{src_in_pod.split("/")[-1]}'
+                                kubectl_cp_from_slate_controller_to_host(src_in_pod, dst_in_host)
+                                
+                            else:
+                                print("???")
+                                print(f"mode: {mode} is not supported")
+                                exit()
+                            '''end of one set of experiment'''
+                            # restart_deploy(exclude=[])
                             
                             per_wrk_et = datetime.now()
                             per_wk_duration = (per_wrk_et - per_wrk_st).seconds
