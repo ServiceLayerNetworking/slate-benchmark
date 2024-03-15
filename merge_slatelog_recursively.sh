9dffb40cc7e31a1f8e891e7055f5eac8b1da6942
gangmuk@gmail.com <gangmuk@gmail.com>
2024-03-15 16:36:42 +0000

    Update merge_slatelog_recursively.sh

--- merge_slatelog_recursively.sh
+++ merge_slatelog_recursively.sh
@@ -1,2 +1,8 @@
-output_filename=$1
-find . -type f -name "*.slatelog" -exec cat {} + > ${output_filename}
+directory=$1
+if [ -z "$directory" ]; then
+    echo "Usage: merge_slatelog_recursively.sh <directory>"
+    exit 1
+fi
+output_filename="merged.slatelog"
+find "${directory}" -type f -name "*.slatelog" -exec cat {} + > ${output_filename}
+echo "* output: ${output_filename}"
